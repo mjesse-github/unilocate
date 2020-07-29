622de5ce425accd53dd3784643a88390bfc8b7db
papajesse <59729734+papajesse@users.noreply.github.com>
2020-07-29 15:29:38 +0300

    Add files via upload

---
+++ index/unilocate/script.js
@@ -0,0 +1,54 @@
+function alusta()
+  {
+    document.getElementById("kyssa").innerHTML  = "Kas sulle meeldivad humanitaar- või reaalteadused?";
+    document.getElementById("alusta").style.display = 'none';
+    document.getElementById("r1").style.display = 'inline';
+    document.getElementById('h1').style.display = 'inline';
+    document.getElementById('v1').style.display = 'inline';
+  }
+
+function n1()
+  {
+    document.getElementById("kyssa").innerHTML  ="Kas sulle meeldib lugeda?";
+    document.getElementById("n11").style.display = 'none';
+    document.getElementById('n12').style.display = 'none';
+    document.getElementById('n13').style.display = 'none';
+    document.getElementById('n21').style.display = 'inline';
+    document.getElementById('n22').style.display = 'inline';
+  }
+
+function n2()
+  {
+    document.getElementById("kyssa").innerHTML  ="Kas sa sooviksid rohkem inimesi aidata või midagi luua";
+    document.getElementById("n21").style.display = 'none';
+    document.getElementById('n22').style.display = 'none';
+    document.getElementById('n31').style.display = 'inline';
+    document.getElementById('n32').style.display = 'inline';
+  }
+
+function n3()
+  {
+    document.getElementById("kyssa").innerHTML  ="Kas oskad hästi kriitiliselt mõelda?";
+    document.getElementById("n31").style.display = 'none';
+    document.getElementById('n32').style.display = 'none';
+    document.getElementById('n41').style.display = 'inline';
+    document.getElementById('n42').style.display = 'inline';
+  }
+
+function n2()
+  {
+    document.getElementById("kyssa").innerHTML  ="Kas sa sooviksid rohkem inimesi aidata või midagi luua";
+    document.getElementById("n41").style.display = 'none';
+    document.getElementById('n42').style.display = 'none';
+    document.getElementById('n51').style.display = 'inline';
+    document.getElementById('n52').style.display = 'inline';
+  }
+
+  function n3()
+    {
+      document.getElementById("kyssa").innerHTML  ="Kas oskad hästi kriitiliselt mõelda?";
+      document.getElementById("n51").style.display = 'none';
+      document.getElementById('n52').style.display = 'none';
+      document.getElementById('n61').style.display = 'inline';
+      document.getElementById('n62').style.display = 'inline';
+    }
